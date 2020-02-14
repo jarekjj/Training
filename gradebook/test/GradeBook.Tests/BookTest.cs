7aeda35199dcb0c2f627c573b58ecad047a6bdac
jarekjj <jarekjj@gmail.com>
2020-02-14 13:21:18 +0000

    change

--- gradebook/test/GradeBook.Tests/BookTest.cs
+++ gradebook/test/GradeBook.Tests/BookTest.cs
@@ -3,14 +3,14 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-    public class UnitTest1
+    public class BookTest
     {
         [Fact]
         public void Test1()
         {
             var x = 5;
             var y = 2;
-            var expected = 7;
+            var expected = 10;
             var actual = x * y;
 
             Assert.Equal(expected, actual);
